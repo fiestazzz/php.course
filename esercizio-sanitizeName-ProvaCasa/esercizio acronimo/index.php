1fc804fbff737bc3c7753bb0a85212abd4a01f66
fiestazzz <salvatore.botta@outlook.com>
2021-02-25 11:13:07 +0100

    implementata interazione con html

--- esercizio-sanitizeName-ProvaCasa/esercizio acronimo/index.php
+++ esercizio-sanitizeName-ProvaCasa/esercizio acronimo/index.php
@@ -7,7 +7,7 @@ $dataset = array(
     );
 
 
-    foreach ($dataset as $key => $row)
+   /* foreach ($dataset as $key => $row)
     {
        $valore=$row[0];
        $atteso=$row[1];
@@ -25,6 +25,58 @@ $dataset = array(
     }
 
 
-
+*/
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    .card-pass
+    {
+        color:#2d6a4f; 
+        background-color:#c7f9cc ;
+        border: 2px solid #80ed99;
+        margin-top: 10px;
+        height: 60px;
+        width: 300px;
+    }
+    .card-fail
+    {
+        color:#d90429; 
+        background-color:#EF8182 ;
+        border: 2px solid #d90429;
+        margin-top: 10px;
+        height: 60px;
+        width: 300px;
+    }
+    </style>
+</head>
+<body>
+<?php  
+    foreach ($dataset as $key => $row)
+    {
+        $valore=$row[0];
+        $atteso=$row[1];
+        $linea=$row[2];
+    
+        $result=acronimo($valore);
+        if ($result == $atteso)
+        {
+            echo "<div class='card-pass'>'Pass ,tutto ok !<br> Stringa da testare: '$atteso'(".gettype($atteso).")<br> Stringa trasformata: $result (".gettype($result).") </div>"  ;                    
+        }
+        else
+       {
+           echo  "<div class=\'card-fail\'> 'FAIL - test fallito linea: $linea \n';</div>" ;            
+           //var_dump($result);
+            
+       }
+    }
+?>
+</body>
+</html>
--- esercizio-sanitizeName-ProvaCasa/index.php
+++ esercizio-sanitizeName-ProvaCasa/index.php
@@ -25,7 +25,7 @@ $dataset = [
 
 
 
-foreach ($dataset as $key => $row)
+/*foreach ($dataset as $key => $row)
 {
     $test=$row[0];
     $atteso=$row[1];
@@ -34,7 +34,7 @@ foreach ($dataset as $key => $row)
     $result=sanitizeName($test);
     if ($result == $atteso)
     {
-        //echo "Pass ,tutto ok \n";
+        echo "Pass ,tutto ok \n";
     }
     else
    {
@@ -42,5 +42,56 @@ foreach ($dataset as $key => $row)
        //var_dump($result);
         
    }
-}
+}*/
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    .card-pass
+    {
+        color:#2d6a4f; 
+        background-color:#c7f9cc ;
+        border: 2px solid #80ed99;
+        margin-top: 10px;
+        height: 60px;
+        width: 300px;
+    }
+    .card-fail
+    {
+        color:#d90429; 
+        background-color:#EF8182 ;
+        border: 2px solid #d90429;
+        margin-top: 10px;
+        height: 60px;
+        width: 300px;
+    }
+    </style>
+</head>
+<body>
+<?php  
+    foreach ($dataset as $key => $row)
+    {
+        $test=$row[0];
+        $atteso=$row[1];
+        $linea=$row[2];
+    
+        $result=sanitizeName($test);
+        if ($result == $atteso)
+        {
+            echo "<div class='card-pass'>'Pass ,tutto ok !<br> Stringa da testare: '$atteso'(".gettype($atteso).")<br> Stringa trasformata: $result (".gettype($result).") </div>"  ;                    
+        }
+        else
+       {
+           echo  "<div class=\'card-fail\'> 'FAIL - test fallito linea: $linea \n';</div>" ;            
+            
+            
+       }
+    }
+?>
+</body>
+</html>
