b7db9c893ced55f2b8b9a11a41ea39a34a3943f8
fiestazzz <Salvatore.Botta@outlook.com>
2021-04-20 21:39:28 +0200

    sistemato errore

--- USM(User-Managment_System)/mia_versione/classes/Validatore.php
+++ USM(User-Managment_System)/mia_versione/classes/Validatore.php
@@ -15,12 +15,92 @@ class Validatore
     private $classeBootstrapInputNome;
     private $classeBootstrapFeedbackNome;
 
+    private $classeBootstrapInputCognome;
+    private $classeBootstrapFeedbackCognome;
+
     public function __construct(User $user) {
         $this->user=$user;
         $this->validatore();
     }
 
 
+
+
+
+    public function validatore()
+    {
+        $this->validatoreNome();
+        if($this->controlloNome === true)
+        {
+            $this->messaggioNome ="Nome Corretto";
+            $this->classeBootstrapInputNome="is-valid";
+            $this->classeBootstrapFeedbackNome="valid-feedback";
+        }
+        else
+        {
+            $this->messaggioNome ="Nome Obbligatorio";
+            $this->classeBootstrapInputNome="is-invalid";
+            $this->classeBootstrapFeedbackNome="invalid-feedback";
+        }
+        $this->validatoreCognome();
+        if($this->controlloCognome === true)
+        {
+            $this->messaggioCognome ="Cognome Corretto";
+            $this->classeBootstrapInputCognome="is-valid";
+            $this->classeBootstrapFeedbackCognome="valid-feedback";
+        }
+        else
+        {
+            
+            $this->messaggioCognome ="Cognome Obbligatorio";
+            $this->classeBootstrapInputCognome="is-invalid";
+            $this->classeBootstrapFeedbackCognome="invalid-feedback";
+        }
+    }
+
+    public function validatoreNome()
+    {
+        if(trim($this->user->getFirstName() !== ""))
+        {
+            $this->controlloNome=true;
+        }
+        else
+        {
+            $this->controlloNome=false;
+        }
+    }
+
+    public function validatoreCognome()
+    {
+        if(trim($this->user->getLastName() !== ""))
+        {
+            $this->controlloCognome=true;
+        }
+        else
+        {
+            $this->controlloCognome=false;
+        }
+    }
+
+    public function validatoreEmail()
+    {
+        if(trim($this->user->getEmail() !== ""))
+        {
+           if(filter_var($this->user->getEmail(),FILTER_VALIDATE_EMAIL))
+           {
+               $this->controlloEmail=true;
+           }
+        }
+        else
+        {
+            $this->controlloEmail=false;
+        }
+    }
+
+
+
+
+    
      /**
      * Get the value of messaggioNome
      */ 
@@ -72,78 +152,6 @@ class Validatore
     }
 
 
-
-
-
-
-    public function validatore()
-    {
-        $this->validatoreNome();
-        if($this->controlloNome === true)
-        {
-            $this->messaggioNome ="Nome Corretto";
-            $this->classeBootstrapInputNome="is-valid";
-            $this->classeBootstrapFeedbackNome="valid-feedback";
-        }
-        else
-        {
-            $this->messaggioNome ="Nome Obbligatorio";
-            $this->classeBootstrapInputNome="is-invalid";
-            $this->classeBootstrapFeedbackNome="invalid-feedback";
-        }
-        $this->validatoreCognome();
-        if($this->controlloCognome === true)
-        {
-            
-        }
-    }
-
-    public function validatoreNome()
-    {
-        if(trim($this->user->getFirstName() !== ""))
-        {
-            $this->controlloNome=true;
-        }
-        else
-        {
-            $this->controlloNome=false;
-        }
-    }
-
-    public function validatoreCognome()
-    {
-        if(trim($this->user->getLastName() !== ""))
-        {
-            $this->controlloCognome=true;
-        }
-        else
-        {
-            $this->controlloCognome=false;
-        }
-    }
-
-    public function validatoreEmail()
-    {
-        if(trim($this->user->getEmail() !== ""))
-        {
-           if(filter_var($this->user->getEmail(),FILTER_VALIDATE_EMAIL))
-           {
-               $this->controlloEmail=true;
-           }
-        }
-        else
-        {
-            $this->controlloEmail=false;
-        }
-    }
-
-
-
-
-
-   
-
-
     /**
      * Get the value of classeBootstrapInputNome
      */ 
@@ -166,6 +174,22 @@ class Validatore
     public function getUser()
     {
         return $this->user;
+    }
+
+    /**
+     * Get the value of classeBootstrapInputCognome
+     */ 
+    public function getClasseBootstrapInputCognome()
+    {
+        return $this->classeBootstrapInputCognome;
+    }
+
+    /**
+     * Get the value of classeBootstrapFeedbackCognome
+     */ 
+    public function getClasseBootstrapFeedbackCognome()
+    {
+        return $this->classeBootstrapFeedbackCognome;
     }
 }
 
